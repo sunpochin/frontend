5754a7f085afc698ff2adccab50b84267fa17a2a
sun pochin <sunpochin@gmail.com>
2022-01-24 14:53:40 +0800

    updating 17-http and 11 food order

--- academind/17-http/src/components/Cart/Cart.js
+++ academind/17-http/src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useContext, useState } from 'react';
 
 import Modal from '../UI/Modal';
@@ -8,6 +9,8 @@ import Checkout from './Checkout';
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -25,14 +28,20 @@ const Cart = (props) => {
     setIsCheckout(true);
   }
 
-  const submitOrderHandler = (userData) => {
-    fetch('https://react-http-7bc61-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json', {
+  const submitOrderHandler = async(userData) => {
+    setIsSubmitting(true);
+
+    const response = await fetch('https://react-http-7bc61-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json', {
       method: 'POST',
       body: JSON.stringify({
         user: userData,
         orderedItems: cartCtx.items,
       }),
     });
+
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
   }
 
   const cartItems = (
@@ -59,8 +68,8 @@ const Cart = (props) => {
     </div>
   )
 
-  return (
-    <Modal onClose={props.onClose}>
+  const cartModalContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -68,8 +77,25 @@ const Cart = (props) => {
       </div>
       {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose}/>}
       {!isCheckout && modalActions}
-      
-      
+    </React.Fragment>
+    );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+  const didSubmitModalContent = (
+    <React.Fragment>
+      <p>Successfully sent the order!</p>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </React.Fragment>
+  );
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
--- academind/17-http/src/store/CartProvider.js
+++ academind/17-http/src/store/CartProvider.js
@@ -55,6 +55,9 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if ('CLEAR' === action.type) {
+    return defaultCartState;
+  }
   return defaultCartState;
 };
 
@@ -72,11 +75,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
+  const clearCartHandler = (id) => {
+    dispatchCartAction({ type: 'CLEAR'});
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
