41c1fbe95b5000d413bac2d5945be03f8958b143
sun pochin <sunpochin@gmail.com>
2022-01-27 19:40:03 +0800

    added ui slice

--- academind/19-advanced/src/components/Shop/Products.js
+++ academind/19-advanced/src/components/Shop/Products.js
@@ -1,19 +1,42 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+	{
+		id: 'p1',
+		price: 6,
+		title: 'my first book',
+		description: 'The first book I ever wrote',
+	},
+	{
+		id: 'p2',
+		price: 5,
+		title: 'my 2nd book',
+		description: 'The 2nd book I wrote',
+	},
+];
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{DUMMY_PRODUCTS.map((product) => (
+					<ProductItem
+						title={product.title}
+						price={product.price}
+						description={product.description}
+					/>
+				))}
+
+				{/* <ProductItem
+					title='Test'
+					price={6}
+					description='This is a first product - amazing!'
+				/> */}
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
---
+++ academind/19-advanced/src/store/index.js
@@ -0,0 +1,13 @@
+import uiSlice from './ui-slice';
+import cartSlice from './cart-slice';
+
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+	reducer: {
+		ui: uiSlice.reducer,
+		cart: cartSlice.reducer,
+	},
+});
+
+export default store;
