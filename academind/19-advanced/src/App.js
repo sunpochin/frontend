4dfb9d5d7f11d05f772caa9b07b50d6229c5c298
sun pochin <sunpochin@gmail.com>
2022-01-29 21:48:55 +0800

    finish fetching data

--- academind/19-advanced/src/App.js
+++ academind/19-advanced/src/App.js
@@ -6,18 +6,22 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
 
 function App() {
-	const dispatch = useDispatch();
+	const dispatchxx = useDispatch();
 	const showCart = useSelector((state) => {
-		console.log('state: ', state);
+		// console.log('state: ', state);
 		return state.ui.cartIsVisible;
 	});
 	const cart = useSelector((state) => state.cart);
 	const notification = useSelector((state) => state.ui.notification);
+
+	useEffect(() => {
+		dispatchxx(fetchCartData());
+	}, [dispatchxx]);
 
 	// this useEffect can be in other components too.
 	useEffect(() => {
@@ -26,9 +30,11 @@ function App() {
 			isInitial = false;
 			return;
 		}
-		dispatch(sendCartData(cart));
+		if (cart.changed) {
+			dispatchxx(sendCartData(cart));
+		}
 		// sendCartData().catch((err) => {});
-	}, [cart, dispatch]);
+	}, [cart, dispatchxx]);
 
 	return (
 		<Fragment>
--- academind/19-advanced/src/store/cart-slice.js
+++ academind/19-advanced/src/store/cart-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
 	name: 'cart',
@@ -7,12 +6,18 @@ const cartSlice = createSlice({
 		items: [],
 		totalQuantity: 0,
 		totalAmount: 0,
+		changed: false,
 	},
 	reducers: {
+		replaceCart: (state, action) => {
+			state.totalQuantity = action.payload.totalQuantity || 0;
+			state.items = action.payload.items || [];
+		},
 		addItemToCart: (state, action) => {
 			const newItem = action.payload;
 			const existingItem = state.items.find((item) => item.id === newItem.id);
 			state.totalQuantity++;
+			state.changed = true;
 			if (!existingItem) {
 				state.items.push({
 					id: newItem.id,
@@ -30,6 +35,7 @@ const cartSlice = createSlice({
 			const id = action.payload;
 			const existingItem = state.items.find((item) => item.id === id);
 			state.totalQuantity--;
+			state.changed = true;
 			if (existingItem.quantity === 1) {
 				state.items = state.items.filter((item) => item.id !== id);
 			} else {
@@ -39,51 +45,6 @@ const cartSlice = createSlice({
 		},
 	},
 });
-
-export const sendCartData = (cart) => {
-	return async (dispatch) => {
-		dispatch(
-			uiActions.showNotification({
-				status: 'pending',
-				title: 'Sending...',
-				message: 'Sending cart data to server...',
-			})
-		);
-
-		const sendRequest = async () => {
-			const response = await fetch(
-				'https://react-http-7bc61-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
-				{
-					method: 'PUT',
-					body: JSON.stringify(cart),
-				}
-			);
-			if (!response.ok) {
-				throw new Error('Sending cart data failed!');
-			}
-		};
-
-		try {
-			await sendRequest();
-			dispatch(
-				uiActions.showNotification({
-					status: 'success',
-					title: 'Success!',
-					message: 'Sent cart data successfully!',
-				})
-			);
-		} catch (err) {
-			dispatch(
-				uiActions.showNotification({
-					status: 'error',
-					title: 'Error!',
-					message: 'Sending cart data failed!',
-				})
-			);
-		}
-		// const responseData = await response.json();
-	};
-};
 
 export default cartSlice;
 
