30ed6ba6b4b022c473e877b3d2e1b6eb6642dc00
pac <sunpochin@gmail.com>
2022-05-31 22:13:50 +0800

    using Link to

--- acade/reactjs/20-router/src/components/MainHeader.js
+++ acade/reactjs/20-router/src/components/MainHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MainHeader = () => {
 	return (
@@ -6,10 +7,10 @@ const MainHeader = () => {
 			<nav>
 				<ul>
 					<li>
-						<a href='/welcome'>Welcome</a>
+						<Link to='/welcome'>Welcome</Link>
 					</li>
 					<li>
-						<a href='/products'>Products</a>
+						<Link to ='/products'>Products</Link>
 					</li>
 				</ul>
 			</nav>
