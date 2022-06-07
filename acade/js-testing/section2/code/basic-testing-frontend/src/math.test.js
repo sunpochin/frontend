729daa5f9e1562261fc01e3692f194d3d6369c6c
pac <sunpochin@gmail.com>
2022-06-08 00:01:01 +0800

    adding math.test.js

---
+++ acade/js-testing/section2/code/basic-testing-frontend/src/math.test.js
@@ -0,0 +1,9 @@
+// import { test } from 'vitest';
+import { it, expect} from 'vitest';
+
+import { add } from './math';
+
+it('should summarize all number values in an array', ()=>{
+  const result = add([1, 2, 3]);
+  expect(result).toEqual(5);
+});
