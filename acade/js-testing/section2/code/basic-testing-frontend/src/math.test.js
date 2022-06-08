3fc8ef95418c7f480017da901ddbd34f2fc43aff
pac <sunpochin@gmail.com>
2022-06-08 14:10:38 +0800

    adding tests failing to mark original function

--- acade/js-testing/section2/code/basic-testing-frontend/src/math.test.js
+++ acade/js-testing/section2/code/basic-testing-frontend/src/math.test.js
@@ -3,7 +3,36 @@ import { it, expect} from 'vitest';
 
 import { add } from './math';
 
-it('should summarize all number values in an array', ()=>{
-  const result = add([1, 2, 3]);
-  expect(result).toEqual(5);
+it('should summarize all number values in an array', () => {
+  // Arrange
+  const numbers = [1, 2, 3];
+  const expectedResult = numbers.reduce(
+    (prevValue, currentValue) => prevValue + currentValue, 0);
+  // Act
+  const result = add(numbers);
+  // Assert
+  expect(result).toEqual(expectedResult);
 });
+
+it('should yield NaN if at least one invalid number is provided.', () => {
+	// Arrange
+  const inputs = ['invalid', 1]
+	// Act
+  const result = add(inputs);
+	// Assert
+  expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided.', () => {
+	// Arrange
+	const numbers = ['2', '1'];
+	// Act
+	const result = add(numbers);
+	// Assert
+  const expectedResult = numbers.reduce(
+		(prevValue, currentValue) => +prevValue + +currentValue,
+		0
+	);
+	expect(result).toBe(expectedResult);
+});
+
