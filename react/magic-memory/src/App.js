dd335b943023f2c6f8e0e30c80f9b7277d65a92b
pac <sunpochin@gmail.com>
2022-06-10 20:53:32 +0800

    adding setcards

--- react/magic-memory/src/App.js
+++ react/magic-memory/src/App.js
@@ -4,12 +4,12 @@ import { Card } from './components/Card';
 import './App.css';
 
 const cardImages = [
-	{ src: '/img/helmet-1.png' },
-	{ src: '/img/potion-1.png' },
-	{ src: '/img/ring-1.png' },
-	{ src: '/img/scroll-1.png' },
-	{ src: '/img/shield-1.png' },
-	{ src: '/img/sword-1.png' },
+	{ src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+	{ src: '/img/ring-1.png', matched: false },
+	{ src: '/img/scroll-1.png', matched: false },
+	{ src: '/img/shield-1.png', matched: false },
+	{ src: '/img/sword-1.png', matched: false },
 ];
 
 function App() {
@@ -39,6 +39,11 @@ function App() {
     console.log('choiceOne: ', choiceOne, ', choiceTwo: ', choiceTwo);
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            return card;
+          });
+        })
         console.log('same!');
       } else {
         console.log('not same!');
